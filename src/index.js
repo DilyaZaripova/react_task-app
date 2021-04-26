1ed20dffc2a0d72231f1420ab2246309e3ede3cd
DilyaZaripova <fooont@bk.ru>
2021-04-26 12:23:55 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,129 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import '@atlaskit/css-reset';
+import styled from 'styled-components';
+import { DragDropContext } from 'react-beautiful-dnd';
+import initialData from './initial-data';
+import Column from './column';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Container = styled.div`
+    display: flex;
+`;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+    state = initialData;
+
+    onDragStart = start => {
+        const homeIndex = this.state.columnOrder.indexOf(start.source.droppableId);
+
+        this.setState({
+            homeIndex,
+        });
+    }
+
+    
+
+    
+    onDragEnd = result => {
+        
+        this.setState({
+            homeIndex: null
+        })
+
+       const {destination,source, draggableId} = result;
+
+       if (!destination) {
+           return;
+       }
+
+       if (
+           destination.droppableId === source.droppableId &&
+           destination.index === source.index
+       ) {
+           return;
+       }
+
+       const start = this.state.columns[source.droppableId];
+       const finish = this.state.columns[destination.droppableId];
+
+       if (start === finish) {
+        const newTasksIds = Array.from(start.taskIds);
+        newTasksIds.splice(source.index, 1);
+        newTasksIds.splice(destination.index, 0, draggableId);
+ 
+        const newColumn = {
+            ...start,
+            taskIds: newTasksIds,
+        };
+ 
+        const newState = {
+            ...this.state,
+            columns: {
+                ...this.state.columns,
+                [newColumn.id]: newColumn,
+            },
+        };
+ 
+        this.setState(newState);
+        return;
+     }
+
+     //moving from one list to another
+     const startTaskIds = Array.from(start.taskIds);
+     startTaskIds.splice(source.index, 1);
+     const newStart = {
+         ...start,
+         taskIds: startTaskIds,
+     };
+
+     const finishTaskIds = Array.from(finish.taskIds);
+     finishTaskIds.splice(destination.index,0, draggableId);
+     const newFinish = {
+         ...finish,
+         taskIds: finishTaskIds,
+     };
+
+     const newState = {
+         ...this.state,
+         columns: {
+             ...this.state.columns,
+             [newStart.id]: newStart,
+             [newFinish.id]: newFinish,
+         }
+     };
+     this.setState(newState);
+};
+
+       
+
+    render() {
+        return (
+            <DragDropContext 
+                onDragStart={this.onDragStart}
+                onDragEnd={this.onDragEnd}
+            >
+                <Container>
+                {this.state.columnOrder.map((columnId, index) => {
+                const column = this.state.columns[columnId];
+                const tasks = column.taskIds.map(
+                    taskId => this.state.tasks[taskId]
+                );
+                
+                const isDropDisabled = index < this.state.homeIndex;
+
+                return (
+                    <Column
+                        key={column.id} 
+                        column={column} 
+                        tasks={tasks}
+                        isDropDisabled={isDropDisabled} 
+                    />
+                );
+            })}
+                </Container>
+            </DragDropContext>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
